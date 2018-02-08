f974084ab5427d3a04352ede7ccfa9b30086b3a1
Ethan Uzarowski <ethanmuz@gmail.com>
2018-02-07 23:41:48 -0500

    Got it to print set multiplication

--- Set Multiplication.py
+++ Set Multiplication.py
@@ -1,7 +1,7 @@
 set1works = False;
 set2works = False;
 while (set1works == False):
-    set1 = str(input('Please enter the first set, surrounded by curly braces and separated by commas: {1,2,3}'));
+    set1 = str(input('Please enter the first set, surrounded by curly braces and separated by commas: {1,2,3}\n'));
     if ((set1[0]=='{') & (set1[-1]=='}')):
         set1 = set1[1:-1];
         set1arr = set1.split(',');
@@ -9,7 +9,7 @@ while (set1works == False):
     else:
         print("That input was not a valid set");
 while (set2works == False):
-    set2 = str(input('Please enter the second set, surrounded by curly braces and separated by commas: {1,2,3}'));
+    set2 = str(input('Please enter the second set, surrounded by curly braces and separated by commas: {4,5,6}\n'));
     if ((set2[0]=='{') & (set2[-1]=='}')):
         set2 = set2[1:-1];
         set2arr = set2.split(',');
@@ -19,3 +19,14 @@ while (set2works == False):
 
 print(set1arr);
 print(set2arr);
+
+result = [];
+i=0;
+print(len(set1arr));
+print(len(set2arr));
+for x in range(0, len(set1arr)):
+    for y in range(0, len(set2arr)):
+        print(str(x) + " " + str(y));
+        result.append([set1arr[x],set2arr[y]]);
+        i+=1;
+print(result);
